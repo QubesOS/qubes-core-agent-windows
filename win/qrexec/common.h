33ba8e90f81a8bd4c707d0611e751120c70cff54
Rafał Wojdyła <omeg@omeg.pl>
2013-09-24 14:34:49 +0200

    added .gitignore for binaries, disabled service build for agent

--- win/qrexec/common.h
+++ win/qrexec/common.h
@@ -5,7 +5,7 @@
 #pragma warning(disable:4068)
 #endif
 
-#define BUILD_AS_SERVICE
+//#define BUILD_AS_SERVICE
 
 #define SERVICE_NAME	TEXT("qrexec_agent")
 #define DEFAULT_USER_PASSWORD_UNICODE	L"userpass"
