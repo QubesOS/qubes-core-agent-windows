4b2b2f2d431e5e2e2cba9bdf1980c17df9b8baff
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2013-10-28 01:38:22 +0100

    network-setup: set IP address regardless of interface index
    Previous version with opencoded interface index '14' apparently don't
    always works (not surprising...). Add support for real interface
    enumeration and setting IP addresses. The only thing done via netsh is
    DNS addresses, as IP Helper API doesn't support changing DNS servers.
    Theoretically it is possible via WMI, or directly in registry, but netsh
    is much simpler and also works well.

--- network_setup/qubes-network-setup.c
+++ network_setup/qubes-network-setup.c
@@ -1,13 +1,135 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <xenstore.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
 
 // from service.c
 int service_main(void);
 
+int set_network_parameters(DWORD ip, DWORD netmask, DWORD gateway, PDWORD outInterfaceIndex) {
+	PIP_ADAPTER_INFO	pAdapterInfo;
+	PIP_ADAPTER_INFO	pAdapterInfoCurrent;
+	PIP_ADDR_STRING		pAddrCurrent;
+	MIB_IPINTERFACE_ROW	ipInterfaceRow;
+	ULONG				ulOutBufLen;
+	DWORD				NTEContext, NTEInstance;
+    PMIB_IPFORWARDTABLE	pIpForwardTable = NULL;
+    MIB_IPFORWARDROW	ipfRow;
+    DWORD				dwSize = 0;
+    DWORD				dwRetVal = 0;
+    DWORD				i;
+
+	/* clear this early, to not override dwForwardIfIndex */
+	memset(&ipfRow, 0, sizeof(ipfRow));
+	memset(&ipInterfaceRow, 0, sizeof(ipInterfaceRow));
+
+	ulOutBufLen = 0;
+	if (GetAdaptersInfo( NULL, &ulOutBufLen) != ERROR_BUFFER_OVERFLOW) {
+		    fprintf(stderr, "GetAdaptersInfo call failed with %d\n", GetLastError());
+			goto cleanup;
+	}
+	pAdapterInfo = (IP_ADAPTER_INFO *)malloc (ulOutBufLen);
+
+	if ((dwRetVal = GetAdaptersInfo(pAdapterInfo, &ulOutBufLen)) != ERROR_SUCCESS) {
+		    printf("GetAdaptersInfo call failed with %d\n", dwRetVal);
+			goto cleanup;
+	}
+
+	/* set IP address */
+	pAdapterInfoCurrent = pAdapterInfo;
+	while (pAdapterInfoCurrent) {
+		if (pAdapterInfoCurrent->Type == MIB_IF_TYPE_ETHERNET) {
+			pAddrCurrent = &pAdapterInfoCurrent->IpAddressList;
+			while (pAddrCurrent) {
+				DeleteIPAddress(pAddrCurrent->Context);
+				pAddrCurrent = pAddrCurrent->Next;
+			}
+			dwRetVal = AddIPAddress(ip, netmask, pAdapterInfoCurrent->Index, &NTEContext, &NTEInstance);
+			if (dwRetVal != ERROR_SUCCESS) {
+				fprintf(stderr, "AddIPAddress failed with %d\n", dwRetVal);
+				goto cleanup;
+			}
+			ipfRow.dwForwardIfIndex = pAdapterInfoCurrent->Index;
+			if (outInterfaceIndex)
+				*outInterfaceIndex = ipfRow.dwForwardIfIndex;
+		}
+		pAdapterInfoCurrent = pAdapterInfoCurrent->Next;
+	}
+
+	/* set default gateway */
+    dwRetVal = GetIpForwardTable(NULL, &dwSize, FALSE);
+    if (dwRetVal == ERROR_INSUFFICIENT_BUFFER) {
+        // Allocate the memory for the table
+        pIpForwardTable = (PMIB_IPFORWARDTABLE) malloc(dwSize);
+        if (pIpForwardTable == NULL) {
+            printf("Unable to allocate memory for the IPFORWARDTALE\n");
+			goto cleanup;
+        }
+        // Now get the table.
+        dwRetVal = GetIpForwardTable(pIpForwardTable, &dwSize, FALSE);
+	}
+
+    if (dwRetVal != ERROR_SUCCESS) {
+        fprintf(stderr, "getIpForwardTable failed with %d.\n", dwRetVal);
+        goto cleanup;
+    }
+
+	// Search for the row in the table we want. The default gateway has a destination
+	// of 0.0.0.0. Notice that we continue looking through the table, but copy only
+	// one row. This is so that if there happen to be multiple default gateways, we can
+	// be sure to delete them all.
+    for (i = 0; i < pIpForwardTable->dwNumEntries; i++) {
+        if (pIpForwardTable->table[i].dwForwardDest == 0) {
+            // Delete the old default gateway entry.
+            dwRetVal = DeleteIpForwardEntry(&(pIpForwardTable->table[i]));
+
+            if (dwRetVal != ERROR_SUCCESS) {
+                printf("Could not delete old gateway\n");
+                goto cleanup;
+            }
+        }
+    }
+
+	/* dwForwardIfIndex filled earlier */
+	ipInterfaceRow.Family = AF_INET;
+	ipInterfaceRow.InterfaceIndex = ipfRow.dwForwardIfIndex;
+	dwRetVal = GetIpInterfaceEntry(&ipInterfaceRow);
+	if (dwRetVal != NO_ERROR) {
+		fprintf(stderr, "GetIpInterfaceEntry failed with error %d\n", dwRetVal);
+		goto cleanup;
+	}
+
+
+	ipfRow.dwForwardDest = 0; // default gateway (0.0.0.0)
+	ipfRow.dwForwardMask = 0;
+	ipfRow.dwForwardNextHop = gateway;
+	ipfRow.dwForwardProto = MIB_IPPROTO_NETMGMT;
+	ipfRow.dwForwardMetric1 = ipInterfaceRow.Metric;
+
+	// Create a new route entry for the default gateway.
+    dwRetVal = CreateIpForwardEntry(&ipfRow);
+
+    if (dwRetVal != NO_ERROR)
+        fprintf(stderr, "CreateIpForwardEntry failed with %d\n", dwRetVal);
+
+	dwRetVal = ERROR_SUCCESS;
+
+cleanup:
+    if (pIpForwardTable)
+        free(pIpForwardTable);
+	if (pAdapterInfo)
+		free(pAdapterInfo);
+
+	return dwRetVal;
+}
+
+
+
 int qubes_setup_network() {
 	struct xs_handle *xs;
-	char *interface_name = "14";
+	int interface_index;
 	char *qubes_ip;
 	char *qubes_netmask;
 	char *qubes_gateway;
@@ -36,15 +158,18 @@ int qubes_setup_network() {
 		goto cleanup;
 	}
 
-	_snprintf(cmdline, sizeof(cmdline), "netsh interface ipv4 set address \"%s\" static %s %s %s 1",
-			interface_name, qubes_ip, qubes_netmask, qubes_gateway);
-	if (system(cmdline)!=0) {
-		fprintf(stderr, "Failed to set IP address by calling: %s\n", cmdline);
+	if (set_network_parameters(inet_addr(qubes_ip),
+				inet_addr(qubes_netmask),
+				inet_addr(qubes_gateway),
+				&interface_index) != ERROR_SUCCESS) {
+		/* error already reported */
 		goto cleanup;
 	}
 
-	_snprintf(cmdline, sizeof(cmdline), "netsh interface ipv4 set dnsservers \"%s\" static %s register=none validate=no",
-			interface_name, qubes_gateway);
+	/* don't know how to programatically (and easily) set DNS address, so stay
+	 * with netsh... */
+	_snprintf(cmdline, sizeof(cmdline), "netsh interface ipv4 set dnsservers \"%d\" static %s register=none validate=no",
+			interface_index, qubes_gateway);
 	if (system(cmdline)!=0) {
 		fprintf(stderr, "Failed to set DNS address by calling: %s\n", cmdline);
 		goto cleanup;
