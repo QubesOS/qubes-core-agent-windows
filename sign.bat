3c0640faf1960d37b5ba688d5f7531c7b284098a
Rafał Wojdyła <omeg@invisiblethingslab.com>
2014-09-01 02:35:00 +0200

    build: fixed NO_SIGN case

--- sign.bat
+++ sign.bat
@@ -6,3 +6,4 @@ IF %_BUILDARCH%==x86 (SET ARCHDIR=i386) ELSE (SET ARCHDIR=amd64)
 
 for /R %%f in (*.exe) do %SIGNTOOL% sign /v %CERT_CROSS_CERT_FLAG% /f %CERT_FILENAME% %CERT_PASSWORD_FLAG% /t http://timestamp.verisign.com/scripts/timestamp.dll %%f
 
+:DONT_SIGN
