99619aa761c365e0999ffa50d1154b5eda9ba340
Rafał Wojdyła <omeg@invisiblethingslab.com>
2015-06-01 00:57:32 +0200

    moved functionality to windows-utils

--- src/qrexec-services/common/ioall.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <windows.h>
-
-BOOL FcWriteBuffer(IN HANDLE file, IN const void *buffer, IN DWORD bufferSize);
-BOOL FcReadBuffer(IN HANDLE file, OUT void *buffer, IN DWORD bufferSize);
-BOOL FcCopyUntilEof(IN HANDLE output, IN HANDLE input);
-
-// Returns number of bytes read.
-DWORD FcReadUntilEof(IN HANDLE file, OUT void *buffer, IN DWORD bufferSize);
