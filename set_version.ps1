f0390e33b73ce09fb92c5e3beb1f734b2d43e78b
Rafał Wojdyła <omeg@invisiblethingslab.com>
2015-06-25 00:01:12 +0200

    version 3.0.1

--- set_version.ps1
+++ set_version.ps1
@@ -5,6 +5,6 @@ $version_str = "`"" + $version + "`""
 $version = %{$version -replace "\.", ","}
 $hdr = "#define QTW_FILEVERSION " + $version + "`n"
 $hdr += "#define QTW_FILEVERSION_STR " + $version_str + "`n"
-$hdr += "#define QTW_PRODUCTVERSION 2.0.0.0`n"
-$hdr += "#define QTW_PRODUCTVERSION_STR `"2.0.0.0`"`n"
+$hdr += "#define QTW_PRODUCTVERSION 3.0.0.0`n"
+$hdr += "#define QTW_PRODUCTVERSION_STR `"3.0.0.0`"`n"
 Set-Content -Path "version.h" $hdr
