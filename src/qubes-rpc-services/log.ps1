b322f7e1bab96633a287d35a1457d854d527f4a6
Rafał Wojdyła <omeg@invisiblethingslab.com>
2024-12-03 16:17:43 +0100

    Add simple logging utility for powershell scripts
    Signed-off-by: Rafał Wojdyła <omeg@invisiblethingslab.com>

---
+++ src/qubes-rpc-services/log.ps1
@@ -0,0 +1,44 @@
+function LogStart {
+    $logDir = Get-ItemPropertyValue "hklm:\Software\Invisible Things Lab\Qubes Tools" LogDir
+    $baseName = (Get-Item $MyInvocation.PSCommandPath).BaseName
+    $logname = "$baseName-$(Get-Date -Format "yyyyMMdd-HHmmss")-$PID.log"
+    $global:qwtLogPath = "$logDir\$logName"
+    $global:qwtLogLevel = Get-ItemPropertyValue "hklm:\Software\Invisible Things Lab\Qubes Tools" LogLevel
+}
+
+function Log {
+    param (
+        [ValidateRange(1,5)][int]$level,
+        [string]$msg
+    )
+
+    if ($level -le $qwtLogLevel) {
+        $ts = Get-Date -Format "yyyyMMdd.HHmmss.fff"
+        Add-Content $qwtLogPath -value "[$ts-$("EWIDV"[$level-1])] $msg"
+    }
+}
+
+function LogError {
+    param([string]$msg)
+    Log 1 $msg
+}
+
+function LogWarning {
+    param([string]$msg)
+    Log 2 $msg
+}
+
+function LogInfo {
+    param([string]$msg)
+    Log 3 $msg
+}
+
+function LogDebug {
+    param([string]$msg)
+    Log 4 $msg
+}
+
+function LogVerbose {
+    param([string]$msg)
+    Log 5 $msg
+}
